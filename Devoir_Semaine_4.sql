29e706da0c470a52b5f3bcf99f0e051348fb382a
ejmaltai08 <justinthibo0899@gmail.com>
2024-02-14 09:04:24 -0400

    Devoir_Semaine_4_Exo2

--- Devoir_Semaine_4.sql
+++ Devoir_Semaine_4.sql
@@ -9,3 +9,19 @@ JOIN publishers p ON t.pub_id = p.pub_id
 WHERE p.pub_name = 'Harmattan'
 );
 
+-- Question 2  Obtenir la liste des auteurs dont l'editeur <<Errolles>> a publie tous les livres 
+SELECT au_fname, au_lname
+FROM authors
+WHERE au_id IN 
+   
+(SELECT au_id FROM titleauthor
+JOIN titles ON titleauthor.title_id = titles.title_id
+JOIN publishers ON titles.pub_id = publishers.pub_id
+WHERE pub_name = 'Eyrolles'
+GROUP BY au_id
+HAVING COUNT(DISTINCT titles.title_id) = 
+    
+(SELECT COUNT(DISTINCT titles.title_id) FROM titles 
+WHERE pub_id = (SELECT pub_id FROM publishers WHERE pub_name = 'Eyrolles'))
+);
+    
